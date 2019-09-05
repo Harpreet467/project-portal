a9c36950378f4782c1506810671634b5014496c3
Harpreet <babbubhangu567@gmail.com>
2019-09-05 12:00:51 +0530

    Initial commit

---
+++ backend/app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_cors import CORS
+
+from src.entities.entity import Base, engine
+from src.http.background_jobs import make_celery
+from src.resources.config import REDIS_URL, UPLOAD_FOLDER, MAX_CONTENT_LENGTH
+
+# generate database schema
+Base.metadata.create_all(engine)
+
+app = Flask(__name__)
+CORS(app)
+
+# Redis config with app
+app.config.update(
+    CELERY_BROKER_URL=REDIS_URL,
+    CELERY_RESULT_BACKEND=REDIS_URL
+)
+celery = make_celery(app)
+
+# Upload Media
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_LENGTH
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ backend/src/resources/config.py
@@ -0,0 +1,12 @@
+# DB Configuration
+DB_URL = 'localhost:5432'
+DB_NAME = 'project-portal'
+DB_USER = 'root'
+DB_PASSWORD = 'root'
+
+# Redis Configuration
+REDIS_URL = 'redis://localhost:6379'
+
+# Upload folder path
+UPLOAD_FOLDER = '/path/to/the/uploads'
+MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 100MB
