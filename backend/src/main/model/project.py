a482f22d8f68eb8ff50460c2694b33a0a83d5c4d
inderjit2002 <sak-cloud>
2020-04-29 12:24:52 +0530

    Added more fields in Project

--- backend/src/main/model/project.py
+++ backend/src/main/model/project.py
@@ -8,6 +8,11 @@ class Project(Entity, db.Model):
     title = db.Column(db.String(255))
     description = db.Column(db.Text)
     file_name = db.Column(db.String(255), nullable=True)
+    deadline = db.Column(db.DateTime, nullable=True)
+    city = db.Column(db.String(255))
+    country = db.Column(db.String(255))
+    platform = db.Column(db.String(255), nullable=True)
+    availability = db.Column(db.String(255), nullable=True)
     category = db.relationship(
         'ProjectCategory', secondary=project_to_category, backref=db.backref('projects', lazy='dynamic')
     )
@@ -18,3 +23,8 @@ class Project(Entity, db.Model):
         self.title = kwargs['title']
         self.description = kwargs['description']
         self.proposal_author = kwargs['proposal_author']
+        self.city = kwargs['city']
+        self.country = kwargs['country']
+        self.deadline = kwargs.get('deadline', None)
+        self.platform = kwargs.get('platform', None)
+        self.availability = kwargs.get('availability', None)
